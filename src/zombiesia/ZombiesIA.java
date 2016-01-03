16a6497bcd752d9c1bf60fb78abdcb307ae1aa57
Totoro le Pacha <totorolepacha@gmail.com>
2016-01-04 00:34:39 +0100

    Création de la méthode d'affichage de message dynamique.

--- src/zombiesia/ZombiesIA.java
+++ src/zombiesia/ZombiesIA.java
@@ -5,6 +5,8 @@
  */
 package zombiesia;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.text.WordUtils;
 import zombiesia.Niveau.InvalidValueException;
 
 /**
@@ -24,14 +26,42 @@ public class ZombiesIA {
 		}
 	}
 	
+	
+		
+	
+	
 	public String toString() {
 		return niveau.toString();
 	}
 	
 	public static void main(String[] args) {
-		ZombiesIA jeu = new ZombiesIA(1, 1);
+		ZombiesIA jeu = new ZombiesIA(1, 1);		
+		
+		intro(40, "JEU DE ZOMBIE SUPER LUDIQUE AVEC UNE INTELLIGENCE ARTIFICIELLE QUI DEPASSE L'ENTENDEMENT.",	"Vraiment, elle est révolutionnaire.", "L'algo A* pourrait limite faire le café, et faire du path-finding droit vers votre coeur.");
 		
 		System.out.println(jeu);
 	}
 	
+	private static void intro(int longueur, String... messages) {
+		/*final String[] messages = {"JEU DE ZOMBIE SUPER LUDIQUE AVEC UNE INTELLIGENCE ARTIFICIELLE QUI DEPASSE L'ENTENDEMENT.",
+			"Vraiment, elle est révolutionnaire.",
+			"L'algo A* pourrait limite faire le café, et faire du path-finding droit vers votre coeur."
+		};
+		int longueur = 40; */
+		try {
+			for(String mess:messages) {
+				String[] lines = WordUtils.wrap(mess, longueur, "\n", true).split("\n");
+				System.out.println("--" + StringUtils.repeat(' ', longueur) + "--");
+				for(String l:lines) 
+					System.out.println("--" + StringUtils.center(l, longueur) + "--");
+
+				System.out.println(StringUtils.repeat('-', longueur + 4));
+
+				Thread.sleep(mess.length() * 45);
+			}
+		}
+		catch(Exception e) {
+			
+		}
+	}
 }
