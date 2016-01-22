e7fb21d5b60e730894f966bba874d55e9091cee6
Totoro le Pacha <totorolepacha@gmail.com>
2016-01-22 20:52:04 +0100

    Zombies bloquants ajoutés

--- src/zombiesia/ZombiesIA.java
+++ src/zombiesia/ZombiesIA.java
@@ -39,8 +39,8 @@ public class ZombiesIA {
 		String rep;
 		char dir;
 		while(true) {
-			if(niveau.isArrived()) return true;
-			else if(niveau.isDead()) return false;
+			if(niveau.isDead()) return false;
+			else if(niveau.isArrived()) return true;
 			else {
 				System.out.println(niveau);
 				do {
@@ -82,14 +82,14 @@ public class ZombiesIA {
 		jeu = new ZombiesIA(3);
 		Messages.reponse(Messages.TypeReponse.Niveau3, Boolean.toString(jeu.play()));
 		GroupeMessages.ApresNiveau3.message();
-*/		jeu = new ZombiesIA(4);
+		jeu = new ZombiesIA(4);
 		Messages.reponse(Messages.TypeReponse.Niveau4, Boolean.toString(jeu.play()));
 		GroupeMessages.ApresNiveau4.message();
 		jeu = new ZombiesIA(5);
 		Messages.reponse(Messages.TypeReponse.Niveau5, Boolean.toString(jeu.play()));
 		jeu = new ZombiesIA(6);
 		Messages.reponse(Messages.TypeReponse.Niveau6, Boolean.toString(jeu.play()));
-		jeu = new ZombiesIA(7);
+*/		jeu = new ZombiesIA(7);
 		while(!Messages.reponse(Messages.TypeReponse.Niveau7, Boolean.toString(jeu.play()))) {
 			jeu = new ZombiesIA(7);
 		}
